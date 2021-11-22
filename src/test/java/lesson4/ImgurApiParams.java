655a2ca7652e17888f317f2f357fa18e17ee168d
Lusya0503 <80621934+Lusya0503@users.noreply.github.com>
2021-11-22 12:34:16 +0700

    Delete ImgurApiParams.java

--- src/test/java/lesson4/ImgurApiParams.java
+++
@@ -1,7 +0,0 @@
-package lesson4;
-
-public class ImgurApiParams {
-    public static String TOKEN = "***";
-    public static String API_URL = "https://api.imgur.com";
-    public static String API_VERSION = "3";
-}
